ca02b1a5070692f2dd8d45ce8d2881115c4d137f
steven <8312553+sphred5@users.noreply.github.com>
2022-07-03 10:50:19 -0700

    added global styles and fixed error with featured posts

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import classes from "./hero.module.css";
 
 const Hero = () => {
   return (
-    <section>
-      <div>image</div>
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/profile-picture.png"
+          alt="steven profile picture"
+          width={300}
+          height={300}
+        />
+      </div>
       <h1>Hi I am Steven</h1>
       <p>This is my web development blog</p>
     </section>
