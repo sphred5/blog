ff6f63e991aa70fea188c03667b4ada8a21fdd48
steven <8312553+sphred5@users.noreply.github.com>
2022-07-03 10:20:32 -0700

    added page components and some style

---
+++ components/home-page/hero.js
@@ -0,0 +1,13 @@
+import classes from "./hero.module.css";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>image</div>
+      <h1>Hi I am Steven</h1>
+      <p>This is my web development blog</p>
+    </section>
+  );
+};
+
+export default Hero;
