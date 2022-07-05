196f3c3985ffaea6f05b13732c8ff954f81db015
steven <8312553+sphred5@users.noreply.github.com>
2022-07-05 09:03:48 -0700

    started working on pulling markdown data for individual posts

--- components/post-detail/post-content.js
+++ components/post-detail/post-content.js
@@ -3,20 +3,12 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header.js";
 import classes from "./post-content.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started",
-  image: "getting-started-with-nextjs.png",
-  content: "# This is a first post",
-  date: "2022-10-03",
-};
-
-const PostContent = () => {
-  const imaagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = ({ post }) => {
+  const imaagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imaagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imaagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,22 @@
 import PostConent from "../../components/post-detail/post-content.js";
+import { getPostData } from "../../lib/post-util.js";
 
 const PostDetailPage = () => {
   return <PostConent />;
 };
-
+export function getStaticProps(context) {
+  const { params } = context;
+  return {
+    props: {
+      post: getPostData(params.slug),
+    },
+    revalidate: 600,
+  };
+}
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: false,
+  };
+}
 export default PostDetailPage;
