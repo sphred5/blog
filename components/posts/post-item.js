6581e9d2dcbcdd0e697bd1748f9a286fc2647c78
steven <8312553+sphred5@users.noreply.github.com>
2022-07-04 19:00:23 -0700

    added responsive image, and created all-posts component

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -18,7 +18,13 @@ const PostItem = (props) => {
       <Link href={linkPath}>
         <a>
           <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
